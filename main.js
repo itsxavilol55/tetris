6aedb5847852c565f91424ef23d17cdc800b23f9
Javier Eduardo <javiereduardo55xd@gmail.com>
2024-01-26 19:56:42 -0700

    tablero inicial

---
+++ main.js
@@ -0,0 +1,7 @@
+import './style.css'
+const size = 20;
+const columnas = 24;
+const filas = 30;
+const canvas = document.getElementById("canvas");
+canvas.style.width = size * columnas + "px";
+canvas.style.height = (size * filas) + "px";
