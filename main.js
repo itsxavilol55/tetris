7c0c078d83655184e01d901057e3b2177afe3beb
Javier Eduardo <javiereduardo55xd@gmail.com>
2024-01-27 15:36:33 -0700

    cae pieza

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
 import './style.css'
 const size = 20;
-const columnas = 24;
+const columnas = 21;
 const filas = 30;
+const totalWidth = size * columnas;
+const totalHeight = size * filas;
+let posX = 9;
+let posY = 0;
 const canvas = document.getElementById("canvas");
-canvas.style.width = size * columnas + "px";
-canvas.style.height = (size * filas) + "px";
+const ctx = canvas.getContext("2d");
+canvas.style.width = totalWidth + "px";
+canvas.style.height = totalHeight + "px";
+juego();
+function juego() {
+    posY = 0;
+    caePieza();
+}
+function caePieza() {
+    dibuja();
+    if (compara())
+        return;
+    const promesa = new Promise((resolve) => {
+        setTimeout(() => {
+            borraPieza();
+            resolve();
+        }, 250);
+    });
+    promesa.then(() => caePieza());
+}
+function dibuja() {
+    ctx.beginPath();
+    ctx.fillRect(posX * size, posY * size, size * 2, size);
+}
+function borraPieza() {
+    ctx.clearRect(posX * size, posY * size, size * 2, size);
+    posY++;
+    ctx.closePath();
+}
+function compara() {
+    if (posY * size >= canvas.height - size) {
+        juego();
+        return true;
+    }
+    return false;
+}
+
+
+
