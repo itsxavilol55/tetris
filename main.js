422c0fc5a5b1dd7a7830dc16cacc7b5c1c5d3712
Javier Eduardo <javiereduardo55xd@gmail.com>
2024-01-28 15:47:08 -0700

    borra linea completa

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css'
 const colores = ["#abcdef", "#7ab81a", "#fae3c7", "#1afe4a", "#a4bfea", "#987654", "#fede3a", "#e045ae", "#f02e4a"];
 const size = 20;
-const columnas = 18;
 const filas = 30;
+const columnas = 18;
 const totalWidth = size * columnas;
 const totalHeight = size * filas;
 let posX = 9;
@@ -15,12 +15,19 @@ canvas.style.height = totalHeight + "px";
 juego();
 mueve();
 function juego() {
+    if (posY != 0) comparaLinea();
     const color = Math.round(Math.random() * (colores.length - 1));
     ctx.fillStyle = colores[color];
     posY = 0;
     posX = 9;
     posX2 = 9;
     caePieza();
+}
+function comparaLinea() {
+    const pixeles = ctx.getImageData(0, posY * size, canvas.width, size);
+    const arrayAux = Array.from(pixeles.data);
+    if (!arrayAux.includes(0))
+        ctx.clearRect(0, posY * size, canvas.width, size);
 }
 function caePieza() {
     dibuja();
@@ -38,16 +45,10 @@ function dibuja() {
     ctx.beginPath();
     ctx.fillRect(posX * size, posY * size, size * 2, size);
 }
-function borraPieza() {
-    ctx.clearRect(posX * size, posY * size, size * 2, size);
-    posY++;
-    posX = posX2;
-    ctx.closePath();
-}
 function comparaAbajo() {
     const pixeles = ctx.getImageData(posX * size, (posY + 1) * size, size * 2, size);
     const arrayAux = Array.from(pixeles.data);
-    for (let i = 0; i < arrayAux.length; i++) {
+    for (let i = 0; i < arrayAux.length; i += 20) {
         if (arrayAux[i] != 0) {
             juego();
             return true;
@@ -58,6 +59,12 @@ function comparaAbajo() {
         return true;
     }
     return false;
+}
+function borraPieza() {
+    ctx.clearRect(posX * size, posY * size, size * 2, size);
+    posY++;
+    posX = posX2;
+    ctx.closePath();
 }
 function mueve() {
     document.addEventListener("keydown", function (event) {
@@ -75,7 +82,7 @@ function mueve() {
 function comparaLados(x) {
     const pixeles = ctx.getImageData((posX2 + x) * size, (posY + 1) * size, size * 2, size);
     const arrayAux = Array.from(pixeles.data);
-    for (let i = 0; i < arrayAux.length; i++) {
+    for (let i = 0; i < arrayAux.length; i += 20) {
         if (arrayAux[i] != 0)
             return true;
     }
