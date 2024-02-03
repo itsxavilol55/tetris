2a7fe55dbdb3bf094482371ccea7ef48f2170b50
Javier Eduardo <javiereduardo55xd@gmail.com>
2024-02-03 16:05:58 -0700

    cambiando el tamaño de las piezas

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import './style.css';
 let colores = [];
 const size = 20;
-const filas = 30;
-const columnas = 18;
+const filas = 28;
+const columnas = 16;
 const totalWidth = size * columnas;
 const totalHeight = size * filas;
-let posX = 9;
+let large = 0;
+let posX = columnas / 2;
 let posY = 0;
-let posX2 = 9;
+let posX2 = columnas / 2;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d", { willReadFrequently: true });
 canvas.style.width = totalWidth + "px";
@@ -20,8 +21,9 @@ function juego() {
     const color = Math.round(Math.random() * (colores.length - 1));
     ctx.fillStyle = colores[color];
     posY = 0;
-    posX = 9;
-    posX2 = 9;
+    posX = columnas / 2;
+    posX2 = columnas / 2;
+    large = Math.round(Math.random() * 3) + 1;
     caePieza();
 }
 function comparaLinea() {
@@ -49,16 +51,16 @@ function caePieza() {
         setTimeout(() => {
             borraPieza();
             resolve();
-        }, 160);
+        }, 140);
     });
     promesa.then(() => caePieza());
 }
 function dibuja() {
     ctx.beginPath();
-    ctx.fillRect(posX * size, posY * size, size * 2, size);
+    ctx.fillRect(posX * size, posY * size, size * large, size);
 }
 function comparaAbajo() {
-    const pixeles = ctx.getImageData(posX * size, (posY + 1) * size, size * 2, size);
+    const pixeles = ctx.getImageData(posX * size, (posY + 1) * size, size * large, size);
     const arrayAux = Array.from(pixeles.data);
     for (let i = 0; i < arrayAux.length; i += 20) {
         if (arrayAux[i] != 0) {
@@ -73,7 +75,7 @@ function comparaAbajo() {
     return false;
 }
 function borraPieza() {
-    ctx.clearRect(posX * size, posY * size, size * 2, size);
+    ctx.clearRect(posX * size, posY * size, size * large, size);
     posY++;
     posX = posX2;
     ctx.closePath();
@@ -85,14 +87,14 @@ function mueve() {
             posX2--;
             return;
         }
-        if (tecla == "ArrowRight" && posX2 * size < canvas.width - size * 2 && !comparaLados(1)) {
+        if (tecla == "ArrowRight" && posX2 * size < canvas.width - size * large && !comparaLados(1)) {
             posX2++;
             return;
         }
     });
 }
 function comparaLados(x) {
-    const pixeles = ctx.getImageData((posX2 + x) * size, (posY + 1) * size, size * 2, size);
+    const pixeles = ctx.getImageData((posX2 + x) * size, (posY + 1) * size, size * large, size);
     const arrayAux = Array.from(pixeles.data);
     for (let i = 0; i < arrayAux.length; i += 20) {
         if (arrayAux[i] != 0)
